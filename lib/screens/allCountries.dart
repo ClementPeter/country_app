99bf6e45c70271ca5992d9b99c2a75024b0f76d2
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-13 12:12:54 +0100

    ✨added: Dio dependency and setup

--- lib/screens/allCountries.dart
+++ lib/screens/allCountries.dart
@@ -1,5 +1,6 @@
 import 'package:country/screens/country.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class AllCountries extends StatefulWidget {
   const AllCountries({super.key});
@@ -9,6 +10,27 @@ class AllCountries extends StatefulWidget {
 }
 
 class _AllCountriesState extends State<AllCountries> {
+  //
+  late Future<List?> countries;
+
+  //
+  Future<List?> getCountries() async {
+    try {
+      var response = await Dio().get('https://restcountries.com/v2/all');
+      print(response);
+      return response.data;
+    } catch (e) {
+      print(":::::::::::::$e");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    countries = getCountries();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,21 +38,54 @@ class _AllCountriesState extends State<AllCountries> {
         title: const Text("All Countries"),
       ),
       body: Container(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Country(),
-              ),
+          child: FutureBuilder(
+        future: countries,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data;
+            return ListView.builder(
+              // itemCount: countries.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(data![index]['name'].toString()),
+                      ),
+                    ),
+                  ),
+                );
+              },
             );
-          },
-          child: const Card(
-              child: Text(
-            "Germany",
-          )),
-        ),
-      ),
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )),
     );
   }
 }
+
+
+// ListView(
+//             children: const [
+//               Padding(
+//                 padding: EdgeInsets.all(10.0),
+//                 child: Card(
+//                     child: Padding(
+//                   padding: EdgeInsets.all(8.0),
+//                   child: Padding(
+//                     padding: EdgeInsets.all(8.0),
+//                     child: Text(
+//                       "Germany",
+//                     ),
+//                   ),
+//                 )),
+//               ),
+//             ],
+//           ),
--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Country extends StatelessWidget {
-  const Country({super.key});
+  const Country({super.key, required this.name});
+
+  final String name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(" Country"),
+        title: Text(name),
       ),
       body: Container(
         child: const Card(
